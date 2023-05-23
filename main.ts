bab9a305249bc694ff67181a2f644eaf742107b4
emunfer1411 <130557119+emunfer1411@users.noreply.github.com>
2023-05-23 13:12:58 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,19 +1,28 @@
 let Velocidad = 40
-let Velocidad_de_Giro = 30
+let Velocidad_de_Giro = 40
 let obstaculo = 0
 basic.forever(function () {
     maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, Velocidad)
     if (maqueen.Ultrasonic(PingUnit.Centimeters) < 5) {
         if (obstaculo == 1) {
             maqueen.motorStop(maqueen.Motors.All)
-            maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 0)
+            maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, Velocidad_de_Giro)
+            basic.pause(2000)
             maqueen.motorStop(maqueen.Motors.All)
             obstaculo = 2
         }
         if (obstaculo == 2) {
+            maqueen.motorStop(maqueen.Motors.All)
+            maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, Velocidad_de_Giro)
+            basic.pause(2000)
+            maqueen.motorStop(maqueen.Motors.All)
             obstaculo = 3
         }
         if (obstaculo == 3) {
+            maqueen.motorStop(maqueen.Motors.All)
+            maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, Velocidad_de_Giro)
+            basic.pause(2000)
+            maqueen.motorStop(maqueen.Motors.All)
             obstaculo = 4
         }
         if (obstaculo == 4) {
