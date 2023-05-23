e2ef22271daae7cd54bc36191b90ed323e9cdc85
emunfer1411 <130557119+emunfer1411@users.noreply.github.com>
2023-05-23 13:42:56 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,40 +2,21 @@ let Velocidad = 40
 let Velocidad_de_Giro = 40
 let obstaculo = 0
 basic.forever(function () {
-    maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, Velocidad)
-    if (maqueen.Ultrasonic(PingUnit.Centimeters) < 5) {
+    if (maqueen.Ultrasonic(PingUnit.Centimeters) < 7) {
+        obstaculo = obstaculo + 1
         if (obstaculo == 1) {
             maqueen.motorStop(maqueen.Motors.All)
             maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, Velocidad_de_Giro)
-            basic.pause(2000)
+            basic.pause(1600)
             maqueen.motorStop(maqueen.Motors.All)
-            obstaculo = 2
         }
         if (obstaculo == 2) {
             maqueen.motorStop(maqueen.Motors.All)
             maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, Velocidad_de_Giro)
-            basic.pause(2000)
-            maqueen.motorStop(maqueen.Motors.All)
-            obstaculo = 3
-        }
-        if (obstaculo == 3) {
-            maqueen.motorStop(maqueen.Motors.All)
-            maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, Velocidad_de_Giro)
-            basic.pause(2000)
-            maqueen.motorStop(maqueen.Motors.All)
-            obstaculo = 4
-        }
-        if (obstaculo == 4) {
-            obstaculo = 5
-        }
-        if (obstaculo == 5) {
-            obstaculo = 6
-        }
-        if (obstaculo == 6) {
-            obstaculo = 7
-        }
-        if (obstaculo == 0) {
+            basic.pause(1600)
             maqueen.motorStop(maqueen.Motors.All)
         }
+    } else {
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, Velocidad)
     }
 })
