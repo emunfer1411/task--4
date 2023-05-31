a45202de7579e0e31dbd403adf5ba1dc362ced3f
emunfer1411 <130557119+emunfer1411@users.noreply.github.com>
2023-05-31 12:19:30 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -13,7 +13,31 @@ basic.forever(function () {
         if (obstaculo == 2) {
             maqueen.motorStop(maqueen.Motors.All)
             maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, Velocidad_de_Giro)
-            basic.pause(1600)
+            basic.pause(1800)
+            maqueen.motorStop(maqueen.Motors.All)
+        }
+        if (obstaculo == 3) {
+            maqueen.motorStop(maqueen.Motors.All)
+            maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, Velocidad_de_Giro)
+            basic.pause(2000)
+            maqueen.motorStop(maqueen.Motors.All)
+            obstaculo = 4
+        }
+        if (obstaculo == 4) {
+            maqueen.motorStop(maqueen.Motors.All)
+            maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, Velocidad_de_Giro)
+            basic.pause(1900)
+            maqueen.motorStop(maqueen.Motors.All)
+            obstaculo = 5
+        }
+        if (obstaculo == 5) {
+            maqueen.motorStop(maqueen.Motors.All)
+            maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, Velocidad_de_Giro)
+            basic.pause(1900)
+            maqueen.motorStop(maqueen.Motors.All)
+            obstaculo = 6
+        }
+        if (obstaculo == 6) {
             maqueen.motorStop(maqueen.Motors.All)
         }
     } else {
